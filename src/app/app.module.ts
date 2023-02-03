ac26514c6e278465bdd7f3c6290d763e0cc48fbd
fadingbeat <fadingbeat@gmail.com>
2023-02-03 11:46:49 +0100

    remove imports and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { ContactsTableComponent } from './views/contacts-table/contacts-table.component';
-import { EditContactComponent } from './views/edit-contact/edit-contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ContactsService } from './services/contacts.service';
-import { ErrorDialogService } from './shared/errors/error-dialog.service';
 import { GlobalErrorHandler } from './core/global-error-handler';
 import { CommonModule } from '@angular/common';
-import { LoadingSpinnerComponent } from './shared/spinner/loading-spinner/loading-spinner.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ContactsTableComponent,
-        EditContactComponent,
-        LoadingSpinnerComponent,
-    ],
+    declarations: [AppComponent],
     imports: [
         BrowserModule,
         CommonModule,
@@ -31,8 +21,6 @@ import { LoadingSpinnerComponent } from './shared/spinner/loading-spinner/loadin
         MaterialModule,
     ],
     providers: [
-        ContactsService,
-        ErrorDialogService,
         {
             provide: ErrorHandler,
             useClass: GlobalErrorHandler,
