d3f437cb61ab9a5df964e3edde0d09e5b0991d4b
fadingbeat <fadingbeat@gmail.com>
2023-02-03 12:10:44 +0100

    change old data

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/talentlyft-interview-a01'),
+      dir: require('path').join(__dirname, './coverage/angular-template-project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
